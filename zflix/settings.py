599a2df8d5ceb96942575c84aa26316acfa8eafe
Zouss28 <alanjonse2@gmail.com>
2024-03-24 12:26:05 +0200

    Remove a hardcoded Django secret key

--- zflix/settings.py
+++ zflix/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k1tek&#@)9=5gz*0ze9zi!y9%!f)f^nuqmq---!)7rz2d^r%c%"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
